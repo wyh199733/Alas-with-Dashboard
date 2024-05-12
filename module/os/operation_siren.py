602493b9ae595a71497fecf4caf5726a8616ea72
wuyh <2364225874@qq.com>
2024-05-12 22:58:22 +0800

    短猫条件修改

--- module/os/operation_siren.py
+++ module/os/operation_siren.py
@@ -300,7 +300,7 @@ class OperationSiren(OSMap):
         if self.is_cl1_enabled and self.config.OpsiMeowfficerFarming_ActionPointPreserve < 1000:
             logger.info('With CL1 leveling enabled, set action point preserve to 1000')
             self.config.OpsiMeowfficerFarming_ActionPointPreserve = 1000
-        preserve = min(self.get_action_point_limit(), self.config.OpsiMeowfficerFarming_ActionPointPreserve, 2000)
+        preserve = self.config.OpsiMeowfficerFarming_ActionPointPreserve
         if preserve == 0:
             self.config.override(OpsiFleet_Submarine=False)
         if self.is_cl1_enabled:
